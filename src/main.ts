8b7e4d283b9eef12396c588cbf6e64db35559a64
Ashley Knapp <ajknapp@ucsc.edu>
2024-10-09 17:04:24 -0700

    step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import "./style.css";
 
 // --- Variables ---
 let bogosCount: number = 0;
-const autoClickInterval: number = 1000;
+const clickBogosAmount: number = 1;
 const autoBogosAmount: number = 1;
+const autoClickInterval: number = 1000;
+let lastUpdate: number = performance.now();
+// Some const vars may change later on but for now they are const
+
 
 // --- Page Setup ---
 const app: HTMLDivElement = document.querySelector("#app")!;
@@ -26,20 +30,32 @@ counter.style.paddingTop = "15px";
 counter.innerHTML = `No bogos binted 👽`;
 app.append(counter);
 
-// Auto Clicking
-setInterval(function(){
-    bogosIncrease(autoBogosAmount);
-}, autoClickInterval)
+// -- Auto Clicking --
+// Old method that was affected by framerate
+// setInterval(function () {
+//   bogosIncrease(autoBogosAmount);
+// }, autoClickInterval);
+
+// Automatically increment the counter by autoBogosAmount every 1000ms (autoCLickInterval)
+requestAnimationFrame(interval); // Initial call to start autoclicking process
+function interval(timestamp: number){
+    if (timestamp - lastUpdate >= autoClickInterval){
+        lastUpdate = timestamp;
+        bogosIncrease(autoBogosAmount);
+    }
+    requestAnimationFrame(interval);
+}
+
 
 // --- Event Listening ---
 button.addEventListener("click", function () {
-  bogosIncrease(1);
+  bogosIncrease(clickBogosAmount);
 });
 
 // --- Helper Functions ---
-function bogosIncrease(amount: number){
-    // Increase bogos by amount
-    bogosCount += amount;
+function bogosIncrease(amount: number) {
+  // Increase bogos by amount
+  bogosCount += amount;
   // Update Text
   counter.innerHTML = `${bogosCount} bogos binted 👽`;
 }
