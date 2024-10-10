c056b9d21b2b969bb9b918233f3e70836749010e
Ashley Knapp <ajknapp@ucsc.edu>
2024-10-09 23:42:52 -0700

    Step 9 done

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import "./style.css";
 
+interface Item {
+  name: string,
+  cost: number,
+  rate: number
+};
+
+const availableItems : Item[] = [
+  {name: "Small Binter", cost: 10, rate: 0.1},
+  {name: "Average Binter", cost: 100, rate: 2},
+  {name: "Epic Binter", cost: 1000, rate: 50},
+];
+
 // --- Variables ---
 let bogosCount: number = 0;
 const clickBogosAmount: number = 1;
@@ -9,10 +21,15 @@ let lastUpdate: number = performance.now();
 let upgradeAmt1: number = 0;
 let upgradeAmt2: number = 0;
 let upgradeAmt3: number = 0;
-let upgradeCost1: number = 10;
-let upgradeCost2: number = 100;
-let upgradeCost3: number = 1000;
-// Some const vars may change later on but for now they are const
+let upgradeCost1:number = availableItems[0]["cost"]*Math.pow(1.15, upgradeAmt1);
+let upgradeCost2:number = availableItems[1]["cost"]*Math.pow(1.15, upgradeAmt2);
+let upgradeCost3:number = availableItems[2]["cost"]*Math.pow(1.15, upgradeAmt3);
+// let upgradeCost1: number = 10;
+// let upgradeCost2: number = 100;
+// let upgradeCost3: number = 1000;
+
+
+console.log(availableItems[0]["name"]);
 
 // --- Page Setup ---
 const app: HTMLDivElement = document.querySelector("#app")!;
@@ -37,15 +54,15 @@ clickerBtn.style.fontSize = "100px";
 app.append(clickerBtn);
 
 const upgradeBtn1 = document.createElement("button");
-upgradeBtn1.innerHTML = "Auto Binting x0.1<br>(Cost 10 Bogos)<br>0";
+upgradeBtn1.innerHTML = `${availableItems[0]["name"]} x${availableItems[0]["rate"]}<br>(Cost ${upgradeCost1} Bogos)<br>${upgradeAmt1}`;
 upgradeBtn1.style.fontSize = "20px";
 
 const upgradeBtn2 = document.createElement("button");
-upgradeBtn2.innerHTML = "Auto Binting x2<br>(Cost 100 Bogos)<br>0";
+upgradeBtn2.innerHTML = `${availableItems[1]["name"]} x${availableItems[1]["rate"]}<br>(Cost ${upgradeCost2} Bogos)<br>${upgradeAmt2}`;
 upgradeBtn2.style.fontSize = "20px";
 
 const upgradeBtn3 = document.createElement("button");
-upgradeBtn3.innerHTML = "Auto Binting x50<br>(Cost 1000 Bogos)<br>0";
+upgradeBtn3.innerHTML = `${availableItems[2]["name"]} x${availableItems[2]["rate"]}<br>(Cost ${upgradeCost3} Bogos)<br>${upgradeAmt3}`;
 upgradeBtn3.style.fontSize = "20px";
 
 // upgrade btn app appends/prepends
@@ -68,34 +85,46 @@ clickerBtn.addEventListener("click", function () {
 
 upgradeBtn1.addEventListener("click", function () {
   if (bogosCount >= 10) {
-    upgradeAmt1++;
+    // Deduct cost from total
+    upgradeCost1 = availableItems[0]["cost"]*Math.pow(1.15, upgradeAmt1);
+    bogosDecrease(upgradeCost1);
     autoBogosAmount += 0.1;
-    bogosDecrease(upgradeAmt1);
-    upgradeCost1 *= 1.15;
-    growthLabel.innerHTML = `Current Auto Bint Rate: ${autoBogosAmount.toFixed(1)}`;
-    upgradeBtn1.innerHTML = `Auto Binting x0.1<br>(Cost ${upgradeCost1.toFixed(2)} Bogos)<br>${upgradeAmt1}`;
+    // Calc new cost to show on label
+    upgradeAmt1++;
+    upgradeCost1 = availableItems[0]["cost"]*Math.pow(1.15, upgradeAmt1);
+  checkDisabled();
+  growthLabel.innerHTML = `Current ${availableItems[0]["name"]} Rate: ${autoBogosAmount.toFixed(1)}`;
+    upgradeBtn1.innerHTML = `${availableItems[0]["name"]} x${availableItems[0]["rate"]}<br>(Cost ${upgradeCost1.toFixed(2)} Bogos)<br>${upgradeAmt1}`;
   }
 });
 
 upgradeBtn2.addEventListener("click", function () {
   if (bogosCount >= 100) {
-    upgradeAmt2++;
+    // Deduct cost from total
+    upgradeCost2 = availableItems[1]["cost"]*Math.pow(1.15, upgradeAmt2);
+    bogosDecrease(upgradeCost2);
     autoBogosAmount += 2;
-    bogosDecrease(upgradeAmt2);
-    upgradeCost2 *= 1.15;
-    growthLabel.innerHTML = `Current Auto Bint Rate: ${autoBogosAmount.toFixed(1)}`;
-    upgradeBtn2.innerHTML = `Auto Binting x2<br>(Cost ${upgradeCost2.toFixed(2)} Bogos)<br>${upgradeAmt2}`;
+    // Calc new cost to show on label
+    upgradeAmt2++;
+    upgradeCost2 = availableItems[1]["cost"]*Math.pow(1.15, upgradeAmt2);
+  checkDisabled();
+  growthLabel.innerHTML = `Current ${availableItems[1]["name"]} Rate: ${autoBogosAmount.toFixed(1)}`;
+    upgradeBtn2.innerHTML = `${availableItems[1]["name"]} x${availableItems[1]["rate"]}<br>(Cost ${upgradeCost2.toFixed(2)} Bogos)<br>${upgradeAmt2}`;
   }
 });
 
 upgradeBtn3.addEventListener("click", function () {
   if (bogosCount >= 1000) {
-    upgradeAmt3++;
+    // Deduct cost from total
+    upgradeCost3 = availableItems[2]["cost"]*Math.pow(1.15, upgradeAmt3);
+    bogosDecrease(upgradeCost3);
     autoBogosAmount += 50;
-    bogosDecrease(upgradeAmt3);
-    upgradeCost3 *= 1.15;
-    growthLabel.innerHTML = `Current Auto Bint Rate: ${autoBogosAmount.toFixed(1)}`;
-    upgradeBtn3.innerHTML = `Auto Binting x50<br>(Cost ${upgradeCost3.toFixed(2)} Bogos)<br>${upgradeAmt3}`;
+    // Calc new cost to show on label
+    upgradeAmt3++;
+    upgradeCost3 = availableItems[2]["cost"]*Math.pow(1.15, upgradeAmt2);
+  checkDisabled();
+  growthLabel.innerHTML = `Current ${availableItems[2]["name"]} Rate: ${autoBogosAmount.toFixed(1)}`;
+    upgradeBtn3.innerHTML = `${availableItems[2]["name"]} x${availableItems[2]["rate"]}<br>(Cost ${upgradeCost3.toFixed(2)} Bogos)<br>${upgradeAmt3}`;
   }
 });
 
@@ -130,7 +159,7 @@ function bogosDecrease(amount: number) {
 }
 
 function checkDisabled() {
-  upgradeBtn1.disabled = bogosCount < 10;
-  upgradeBtn2.disabled = bogosCount < 100;
-  upgradeBtn3.disabled = bogosCount < 1000;
+  upgradeBtn1.disabled = bogosCount < upgradeCost1;
+  upgradeBtn2.disabled = bogosCount < upgradeCost2;
+  upgradeBtn3.disabled = bogosCount < upgradeCost3;
 }
